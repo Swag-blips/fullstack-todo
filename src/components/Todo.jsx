e24247578caeee3b96e84200d8e015a28328d0d5
Swag-blips <coderblip@gmail.com>
2024-06-01 11:06:03 +0100

    initial commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import bgLight from "../assets/images/bg-desktop-light.jpg";
+
+const Todo = () => {
+  return (
+    <section>
+      <figure className="w-full">
+        <img src={bgLight} alt="background-img" className="w-full" />
+      </figure>
+    </section>
+  );
+};
+
+export default Todo;
