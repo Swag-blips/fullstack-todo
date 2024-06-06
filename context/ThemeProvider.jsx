4664ff87e7ca615d4e6b81556adef58dc4f85780
Swag-blips <coderblip@gmail.com>
2024-06-06 22:35:24 +0100

    initla theme switch

---
+++ context/ThemeProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ handleThemeSwitch, theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import React, { useState, useContext, useEffect } from "react";
 import Todo from "./components/Todo";
+import { ThemeContext } from "../context/ThemeProvider";
 
 function App() {
+  const { handleThemeSwitch, theme } = useContext(ThemeContext);
   return (
     <div className=" ">
+      <button
+        onClick={handleThemeSwitch}
+        className="z-50 dark:bg-black text-white absolute top-0 left-0"
+      >
+        Dark mode
+      </button>
       <Todo />
     </div>
   );
