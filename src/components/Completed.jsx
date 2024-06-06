2ea813c2876a33e2d1ea4a35cc00282136e35c95
Swag-blips <coderblip@gmail.com>
2024-06-06 17:11:42 +0100

    Linked components

---
+++ src/components/Completed.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Completed = () => {
+  return <div>This is the completed</div>;
+};
+
+export default Completed;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import bgLight from "../assets/images/bg-desktop-light.jpg";
 import sun from "../assets/images/icon-sun.svg";
 import moon from "../assets/images/icon-moon.svg";
-import cross from "../assets/images/icon-cross.svg";
+
+import Active from "./Active";
+import Completed from "./Completed";
 
 const Todo = () => {
+  const [active, setActive] = useState(true);
+  const [completed, setCompleted] = useState(false);
+
+  const toggleToCompleted = () => {
+    setCompleted(true);
+    setActive(false);
+  };
+  const toggleToActive = () => {
+    setCompleted(false);
+    setActive(true);
+  };
   return (
     <section className="flex items-center flex-col justify-center">
       <figure className="w-full md:h-[300px] h-[200px]">
@@ -37,120 +50,28 @@ const Todo = () => {
             <div className="w-[20px] h-[20px] absolute top-1/2 left-[1.4rem] transform -translate-y-1/2 md:w-[24px] md:h-[24px] border-[1px] border-[#E3E4F1] circle-border rounded-full" />
           </div>
         </form>
-        <div className="md:w-[540px] md:h-[439px] w-[327px] overflow-y-scroll relative h-[368px] bg-white mt-[16px] rounded-[8px] shadow-lg">
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
+        <div className="md:w-[540px] md:h-[439px] w-[327px] flex flex-col overflow-hidden bg-white mt-[16px] rounded-[8px] shadow-lg">
+          <div className="flex-grow overflow-y-auto">
+            {active && <Active />}
+            {completed && <Completed />}
           </div>
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-          <div className="mx-[23px] my-[20px] items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-          <div className="mx-[23px]  items-center">
-            <div className="flex flex-row items-center justify-between ">
-              <div className="flex items-center gap-[12px] md:gap-[24px]">
-                <input
-                  type="checkbox"
-                  className="custom-checked md:w-[24px] md:h-[24px] w-[20px] h-[20px]"
-                />
-                <p className="md:text-[18px] text-[#494C6B] text-[12px]">
-                  Jog around the park 3x
-                </p>
-              </div>
-              <img src={cross} alt="cross-img" className="cursor-pointer" />
-            </div>
-            <hr className="md:mt-[20px] mt-[16px] border-[#E3E4F1] mx-[-23px]" />
-          </div>
-
-          <div className="bottom-0 z-50 pt-[20px] pb-[20px] bg-white border-t border-t-[#E3E4F1] w-full sticky">
+          <div className="bottom-0 z-50 pt-[20px] pret-div pb-[20px] bg-white border-t border-t-[#E3E4F1] w-full">
             <div className="flex-row text-[12px] md:text-[14px] tracking-[-0.19px] flex mx-[24px]  items-center justify-between">
               <p className=" text-[#9495A5] text-left">5 items left</p>
               <div className="flex-row hidden md:flex  cursor-pointer font-bold gap-[20px] items-center">
                 <p className="text-[#3A7CFD] hover:text-[#494c6b] ">All</p>
-                <p className="text-[#9495A5] hover:text-[#494c6b]">Active</p>
-                <p className="text-[#9495A5] hover:text-[#494c6b]">Completed</p>
+                <p
+                  onClick={toggleToActive}
+                  className="text-[#9495A5] hover:text-[#494c6b]"
+                >
+                  Active
+                </p>
+                <p
+                  onClick={toggleToCompleted}
+                  className="text-[#9495A5] hover:text-[#494c6b]"
+                >
+                  Completed
+                </p>
               </div>
               <button className="text-[#494C6B]">Clear completed</button>
             </div>
@@ -159,12 +80,23 @@ const Todo = () => {
         <div className="bg-white shadow-lg  md:hidden py-[15px] mt-[16px] w-[327px] h-[48px]">
           <div className="flex text-[14px] gap-[20px] tracking-[-0.19px]  items-center justify-center">
             <p className="text-[#3A7CFD] hover:text-[#494c6b] ">All</p>
-            <p className="text-[#9495A5] hover:text-[#494c6b]">Active</p>
-            <p className="text-[#9495A5] hover:text-[#494c6b]">Completed</p>
+            <p
+              onClick={toggleToActive}
+              className="text-[#9495A5] hover:text-[#494c6b]"
+            >
+              Active
+            </p>
+            <p
+              onClick={toggleToCompleted}
+              className="text-[#9495A5] hover:text-[#494c6b]"
+            >
+              Completed
+            </p>
           </div>
         </div>
-
-        <p className="text-[#9495A5] text-[14px] tracking-[-0.19px] mt-[40px] flex items-center justify-center">Drag and drop to reorder list</p>
+        <p className="text-[#9495A5] text-[14px] tracking-[-0.19px] mt-[40px] flex items-center justify-center">
+          Drag and drop to reorder list
+        </p>
       </section>
     </section>
   );
